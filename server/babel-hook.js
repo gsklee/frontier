0449c5b0ffda8334cf5f832c21549fd1a9fffe7b
G. Kay Lee <balancetraveller+github@gmail.com>
2015-09-10 15:46:03 +0800

    Add Babel hook file

---
+++ server/babel-hook.js
@@ -0,0 +1,15 @@
+/*
+[Babel Hook File](https://babeljs.io/docs/usage/require/)
+=========================================================
+
+Do not use ES6 Import/Export because:
+
+1. The syntax is not yet supported by V8
+2. The require hook relies on overriding the `require` function
+*/
+
+require('babel-core/register');
+
+module.exports = require({
+  [__filename]: './server'
+}[process.argv[1]]);
