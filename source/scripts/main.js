7670ad673b0260f8323d7b304356213f0346e4f3
G. Kay Lee <balancetraveller+github@gmail.com>
2015-09-14 16:34:48 +0800

    Add <App/> boilerplate

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,27 @@
+// App Main Entry
+// ==============
+//
+// Import Modules
+// --------------
+//
+// ### NPM Modules
+
 import 'babel-core/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Define <App/>
+// -------------
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hello, world!</div>
+    );
+  }
+}
+
+// Render <App/>
+// -------------
+
+ReactDOM.render(<App/>, document.getElementById('app'));
